061169d2b148c77161fe9d62d6a3038407abe694
Programmer <peshkov@mbober.ru>
2021-02-10 14:47:08 +0300

    fix title

--- src/resources/js/components/DocumentsComponent.vue
+++ src/resources/js/components/DocumentsComponent.vue
@@ -112,7 +112,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Название изображения</h5>
+                        <h5 class="modal-title">Название документа</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body" v-if="chosenDocument">
